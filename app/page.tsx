5cfec28d661435da74ecb8d3758fdf96932030f3
Davi Moura <davisilvag4mer@gmail.com>
2024-11-15 07:53:55 -0300

    feat: different font sizes

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
             <h1 className="text-lg uppercase font-thin text-slate-400 tracking-widest">
               Davi Moura
             </h1>
-            <h1 className="text-6xl font-black pt-2 pb-5">
+            <h1 className="text-4xl md:text-6xl font-black pt-2 pb-5">
               Full Stack Developer
             </h1>
           </div>
@@ -146,7 +146,7 @@ export default function Home() {
       </div> */}
       <div id="projects" className="py-16 px-10">
         <div className="container mx-auto px-4">
-          <h2 className="text-5xl font-bold mb-12">Public Personal Projects</h2>
+            <h2 className="text-3xl md:text-5xl font-bold mb-12">Public Personal Projects</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8">
             {projects.map((project, index) => (
               <div
